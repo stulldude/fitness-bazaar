7bf7eae59a30e5938259b514cbee34eeb714fa34
William Stull II <williamstull@gmail.com>
2021-06-28 21:46:09 -0700

    Working login OAuth2. Logout has known bug

--- config/passport.js
+++ config/passport.js
@@ -1,38 +1,45 @@
 const passport = require('passport');
-
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const User = require('../models/user');
 
 passport.use(
-    new GoogleStrategy( {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK
+  new GoogleStrategy(
+    // Configuration object
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK
     },
+    // The verify callback function
     function(accessToken, refreshToken, profile, cb) {
-        User.findOne({ googleId: profile.id }).then(async function(user) {
-            if (user) return cb(null, user);
-            try {
-                user = await User.create({
-                    name: profile.displayName,
-                    googleId: profile.id,
-                    email: profile.emails[0].value,
-                    avatar: profile.photos[0].value
-                })
-                return cb(null, user);
-            } catch(err) {
-                return cb(err);
-            }
-        });
-    })
-)
+      // a user has logged in with OAuth...
+      User.findOne({ googleId: profile.id }).then(async function(user) {
+        if (user) return cb(null, user);
+        // We have a new user via OAuth!
+        try {
+          user = await User.create({
+            name: profile.displayName,
+            googleId: profile.id,
+            email: profile.emails[0].value,
+            avatar: profile.photos[0].value
+          });
+          return cb(null, user);
+        } catch (err) {
+          return cb(err);
+        }
+      });
+    }
+  )
+);
 
 passport.serializeUser(function(user, cb) {
-    cb(null, user._id);
+  cb(null, user._id);
 });
 
-passport.deserializeUser(async function(userId, cb) {
+passport.deserializeUser(function(userId, cb) {
   User.findById(userId).then(function(user) {
     cb(null, user);
   });
 });
+
+
--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,8 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : '',
-    failureRedirect : ''
+    successRedirect : '/',
+    failureRedirect : '/'
   }
 ));
 
