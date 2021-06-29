547cb2d4f93d877d2d6555536124d4cbdf8f9431
William Stull II <williamstull@gmail.com>
2021-06-29 15:42:55 -0700

    Able to input data up to exercises

--- controllers/routines.js
+++ controllers/routines.js
@@ -2,13 +2,22 @@ const Routine = require('../models/routine')
 
 module.exports = {
     new: newRoutine,
-    create: create,
     newWorkout: newWorkout,
+    create: create,
+    createWorkout: createWorkout,
+    createExercise: createExercise,
+    showWorkout: showWorkout,
 }
 
 function newRoutine(req, res) {
     console.log('---newRoutine()---');
     res.render('routines/new', {title: "Create Routine"});
+}
+
+function newWorkout(req, res) {
+    console.log('---newWorkout()---')
+    let routineId = req.params.id;
+    res.render('workouts/new', {title: "Add to routine", routineId})
 }
 
 function create(req, res) {
@@ -24,7 +33,31 @@ function create(req, res) {
     })
 }
 
-function newWorkout(req, res) {
-    console.log('---newWorkout()---')
-    res.render('workouts/new', {title: "Add to routine"})
+function createWorkout(req, res) {
+    Routine.findById(req.params.id, function(err, routine) {
+        console.log('inCreateWorkout Routine')
+        routine.workouts.push(req.body);
+        routine.save(function(err) {
+            console.log('in save')
+            res.redirect(`/routines/${routine._id}/workouts/${routine.workouts._id}`)
+        })
+    })
+}
+
+function createExercise(req, res) {
+    Routine.findById(req.params.id, function(err, routine) {
+        routine.workouts.findById(req.params.wid, function(err, workout) {
+            workout.exercises.push(req.body);
+            console.log('hello')
+            routine.save(function(err) {
+                res.redirect(res.redirect(`/routines/${routine._id}/workouts/${workout._id}`))
+            })
+        })
+    });
+}
+
+function showWorkout(req, res) {
+    Routine.findById(req.params.id, function(err, routine) {
+        res.render('workouts/show', {routine, title: routine.name})
+    });
 }
--- routes/routines.js
+++ routes/routines.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const routinesCtrl = require('../controllers/routines.js');
 
 router.get('/new', routinesCtrl.new);
-router.get('/:id/workouts/new', routinesCtrl.newWorkout)
-router.post('/', routinesCtrl.create)
+router.get('/:id/workouts/new', routinesCtrl.newWorkout);
+router.get('/:id/workouts/:wid', routinesCtrl.showWorkout)
+router.post('/:id/workouts', routinesCtrl.createWorkout);
+router.post('/:id/workouts/:wid/exercises', routinesCtrl.createExercise);
+router.post('/', routinesCtrl.create);
 
 module.exports = router;
